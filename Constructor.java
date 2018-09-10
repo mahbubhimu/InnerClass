27dfca8ceb6d07941ecbb6e0bb84addae8362a42
istt <istt.mirpur@gmail.com>
2018-09-10 13:00:09 +0600

    Constructor Study

---
+++ Constructor.java
@@ -0,0 +1,31 @@
+//Constructor in Java
+package javaapplication2;
+import java.util.Scanner;
+class Constructor_Test{
+   private
+           int id;
+           double gpa;
+           String name;
+
+    // Normal Method
+    void print(int a, int b, int c){
+        System.out.println(a+ " "+b+" "+c);
+    }
+    //Constructor without parameter
+    Constructor_Test(){
+        id  = 10;
+        gpa = 3.11;
+        name = "Mahbubur Rahman Chowdhury";
+        System.out.println("ID = " +id+ ", GPA = "+gpa+ ", Name  = "+name);
+    }
+    //Constructor with parameter
+    Constructor_Test(int id,double gpa, String name){
+        System.out.println("ID = " +id+ " GPA = "+gpa+ " Name  = "+name);
+    }
+}
+public class demo{
+    public static void main(String[] args) {
+        Constructor_Test objwp = new Constructor_Test();
+        Constructor_Test obj = new Constructor_Test(8,3.11,"Mamunur Rahman Chowdhury");
+    }
+}
